f136efbeaff4533291b7fd18933a1b7298b5dbd7
IanPC <iancaminhas25@hotmail.com>
2022-01-09 08:26:16 -0300

    Navigation with drawer

--- src/navegacao/index.js
+++ src/navegacao/index.js
@@ -2,13 +2,15 @@ import React from 'react'
 import {SafeAreaView} from 'react-native'
 import {NavigationContainer} from '@react-navigation/native'
 
+import Drawer from './Drawer'
 //import Stack from './Stack'
-import Tab from './Tab'
+//import Tab from './Tab'
 
 export default props =>(
     <SafeAreaView style= {{flex: 1}}>
         <NavigationContainer>
-            <Tab />
+            <Drawer/>
+            {/*<Tab />*/}
             {/*<Stack/>*/}
         </NavigationContainer>
     </SafeAreaView>
---
+++ src/views/TelaD.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import TextoCentral from '../components/TextoCentral'
+import {Button, View} from 'react-native'
+
+export default props =>{
+       return ( 
+            <View style={{ flex: 1 }}>
+               <View style={{ flexDirection: 'row', justifyContent:'flex-end' }}>
+                    <Button 
+                        title='Abrir'
+                        onPress={() => {
+                            props.navigation.openDrawer()
+                            setTimeout(()=>{
+                                props.navigation.openDrawer()
+                                setInterval(()=>{
+                                    props.navigation.toggleDrawer()
+                                }, 1000)
+                            }, 3000)
+                        
+                        }}    
+                    />
+
+               </View>
+           <View style={{ flex: 1 }}>
+                   <TextoCentral corFundo='#33fa72' corTexto='#000'>
+                       Tela D
+                   </TextoCentral>
+               </View>
+               </View>
+
+       )
+}
+   
